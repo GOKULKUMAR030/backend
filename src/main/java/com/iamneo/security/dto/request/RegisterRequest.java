e444ca00e622f00c1580e025f99be7353f10ccc9
GOKULKUMAR030 <114055330+GOKULKUMAR030@users.noreply.github.com>
2023-07-26 10:22:50 +0530

    Add files via upload

---
+++ src/main/java/com/iamneo/security/dto/request/RegisterRequest.java
@@ -0,0 +1,31 @@
+package com.iamneo.security.dto.request;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+	@Id
+	@GeneratedValue
+    private int id;
+	private String firstName;
+	private String lastName;
+	private String address; 
+	private String contactNo; 
+	private String degree;
+	private String specilization;
+	private String passingYear;
+	private float cgpa;
+	private String email;
+	private String password;
+}
